522519c0889ab5130c47c4d7146c5e84a4c88bec
Samuel Lee <lsme8821@gmail.com>
2025-05-19 01:35:44 +0900

    feat: Implement shutdown and disable eth0 interface

--- serve.py
+++ serve.py
@@ -13,10 +13,28 @@ app = Flask(__name__)
 current_files_dir = Path(os.getcwd())
 static_files_dir = current_files_dir / "PSFree"
 exclude_path = ["", "index.html", "favicon.ico"]
+is_debug = False
 
 
-@app.route("/api/<path:path>", methods=["GET"])
+@app.route("/api/<path:path>", methods=["POST"])
 def read_api_routes(path: str):
+    def disable_etho0():
+        logging.error("Disable eth0 interface...")
+        if not is_debug:
+            os.system("ifconfig eth0 down")
+            exit(0)
+
+    def shutdown():
+        logging.error("Shutting down the system...")
+        if not is_debug:
+            os.system("halt")
+            exit(0)
+    
+    if path == "disable-eth0":
+        disable_etho0()
+    elif path == "shutdown":
+        shutdown()
+
     return jsonify({"path": path})
 
 
@@ -34,7 +52,10 @@ def catch_all_routes(path: str = ""):
 @click.option("--port", default=9191, help="Port to run the server on")
 @click.option("--debug", is_flag=True, help="Enable debug mode")
 def main(host: str, port: int, debug: bool):
+    global is_debug
+    is_debug = debug
     app.run(host=host, port=port, debug=debug)
+
 
 if __name__ == "__main__":
     main()
